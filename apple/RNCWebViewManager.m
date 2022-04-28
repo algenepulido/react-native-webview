270fe1eb4933c08e52f78b6c737ec5d516c41021
Algene Pulido <algene.pulido@gmail.com>
2022-04-28 13:58:54 +0800

    fixed: duplicate declaration of skipSslErrors

--- apple/RNCWebViewManager.m
+++ apple/RNCWebViewManager.m
@@ -53,7 +53,6 @@ RCT_EXPORT_MODULE()
   return webView;
 }
 
-RCT_EXPORT_VIEW_PROPERTY(skipSslErrors, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(source, NSDictionary)
 RCT_EXPORT_VIEW_PROPERTY(onFileDownload, RCTDirectEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onLoadingStart, RCTDirectEventBlock)
